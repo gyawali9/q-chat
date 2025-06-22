476e7c639d47d56030488f3068249d1451cdf26b
gyawali9 <roshangyawali9@gmail.com>
2025-06-22 16:54:02 +0545

    fix: eslint

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,22 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  {
-    ignores: ["dist/**", "node_modules/**"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js, "@typescript-eslint": tsPlugin },
-    extends: ["js/recommended", "plugin:@typescript-eslint/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: {
-      parser: "@typescript-eslint/parser",
-      globals: globals.browser,
+export default defineConfig({
+  ignores: ["dist/**", "node_modules/**"],
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+      plugins: { js, "@typescript-eslint": tsPlugin },
+      extends: ["js/recommended", "plugin:@typescript-eslint/recommended"],
+      parser: tsParser,
+      languageOptions: {
+        globals: globals.browser,
+      },
     },
-  },
-]);
+  ],
+});
